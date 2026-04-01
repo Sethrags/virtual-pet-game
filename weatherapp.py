f28d6121e2f718a8413e504eccc57749221179f7
Evan Beitz <enbeitz@gmail.com>
2026-04-01 11:22:30 -0600

    Create weatherapp.py
    basic structure for the weather app. changes will probably be required but right now you should theoretically be able to enter a city and it return the temp as well as a general indication of the "feeling" of it. i'll do some more tests in the future regarding importing it.

---
+++ weatherapp.py
@@ -0,0 +1,47 @@
+import requests
+
+# feel free to adjust these values if you need to
+def classify_temp(temp_f):
+    temp_f = float(temp_f)
+    if temp_f < 50:
+        return "cold"
+    if temp_f <= 80:
+        return "temperate"
+    if temp_f > 80:
+        return "hot"
+
+def get_weather(location):
+    url = f"https://wttr.in/{location}?format=j1"
+    response = requests.get(url)
+
+    if response.status_code == 200: # has internet connection
+        data = response.json()
+
+        # conditions
+        current = data["current_condition"][0]
+        temp = current["temp_F"]
+        desc = current["weatherDesc"][0]["value"]
+        # humidity = current["humidity"]
+        # wind = current["windspeedKmph"]
+        category = classify_temp(temp)
+
+        return {
+            "location": location,
+            "temp": temp,
+            "desc": desc,
+            "category": category
+        }
+    else:
+        return None
+
+# tests
+'''
+        print(f"\nWeather in {location.capitalize()}:")
+        print(f"Temperature: {temp} F {category}")
+        print(f"Conditions: {desc}")
+    else:
+        print("Weather data not found")
+'''
+if __name__ == "__main__":
+    location = input("Please enter a city: ")
+    get_weather(location)
